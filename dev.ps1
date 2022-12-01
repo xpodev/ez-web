c4e2ad66ba6892dea68aeedea1213bb08357c280
Neriya Cohen <neriya@fusionary.co.il>
2022-12-01 15:27:02 +0200

    Optimize dev script

--- dev.ps1
+++ dev.ps1
@@ -1,5 +1,11 @@
-py -m venv .venv
+if (!(Test-Path ./.venv)) {
+  py -m venv .venv
+}
 .venv\Scripts\Activate.ps1
 pip install -r reqs.txt
-echo "import os;import sys;sys.path.insert(0, os.environ['VIRTUAL_ENV'].replace(os.sep + '.venv', '') + os.sep + 'src')" | Out-File -FilePath .venv\Lib\site-packages\dev.pth -Encoding ascii
-uvicorn src.main:Ez.app --reload
+Write-Output "import os;import sys;sys.path.insert(0, os.environ['VIRTUAL_ENV'].replace(os.sep + '.venv', '') + os.sep + 'src')" | Out-File -FilePath .venv\Lib\site-packages\dev.pth -Encoding ascii
+if (Get-Command nodemon -errorAction SilentlyContinue) {
+  nodemon src/main.py
+} else {
+  uvicorn src.main:Ez.app --reload
+}
