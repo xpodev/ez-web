788ebf57ed184a84ccd0df27685a39963bc4c046
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-02-18 12:01:54 +0200

    Add more specific plugin errors

--- modules/plugins/errors.py
+++ modules/plugins/errors.py
@@ -1,11 +1,17 @@
-from ez.errors import EZError
+from ez import EZError
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from plugins.machinery.installer import PluginInstallerId
+    from plugins.machinery.loader import PluginLoaderId
 
 
-class EZPluginManagerError(EZError):
+class EZPluginError(EZError):
     ...
 
 
-class UnknownPluginError(EZPluginManagerError):
+class UnknownPluginError(EZPluginError):
     def __init__(self, plugin_name):
         self.plugin_name = plugin_name
 
@@ -13,9 +19,44 @@ class UnknownPluginError(EZPluginManagerError):
         return f"Unknown plugin: {self.plugin_name}"
 
 
-class PluginAlreadyInstalledError(EZPluginManagerError):
+class PluginAlreadyInstalledError(EZPluginError):
     def __init__(self, plugin_name):
         self.plugin_name = plugin_name
 
     def __str__(self):
         return f"Plugin already installed: {self.plugin_name}"
+
+
+class DuplicateIDError(EZPluginError):
+    def __init__(self, item, id) -> None:
+        self.item = item
+        self.id = id
+
+    def __str__(self):
+        return f"Duplicate ID of {type(self.item).__name__}: {self.id}"
+
+
+class UnknownPluginInstallerError(EZPluginError):
+    def __init__(self, installer_id: "PluginInstallerId") -> None:
+        self._installer_id = installer_id
+
+    def __str__(self):
+        return f"Unknown plugin installer: {self._installer_id}"
+
+
+class UnknownPluginLoaderError(EZPluginError):
+    def __init__(self, loader_id: "PluginLoaderId") -> None:
+        self._loader_id = loader_id
+
+    def __str__(self):
+        return f"Unknown plugin loader: {self._loader_id}"
+
+
+__all__ = [
+    "EZPluginError",
+    "UnknownPluginError",
+    "PluginAlreadyInstalledError",
+    "DuplicateIDError",
+    "UnknownPluginInstallerError",
+    "UnknownPluginLoaderError",
+]
