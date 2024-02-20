58922140f72e66d36ef9e3cd4fd418ec2a1762c4
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-02-20 17:31:44 +0200

    Implement (untested) default plugin installer

--- modules/plugins/builtins/installer.py
+++ modules/plugins/builtins/installer.py
@@ -1,4 +1,13 @@
-from ..errors import EZPluginError
+import yaml
+
+from pathlib import Path
+from zipfile import ZipFile, BadZipFile
+from pydantic import BaseModel, Field, ValidationError
+
+from utilities.version import Version
+
+from ..errors import EZPluginError, PluginAlreadyInstalledError
+from ..plugin_info import PackageName
 from ..machinery.installer import IPluginInstaller, PluginInstallationResult, PluginInstallerInfo
 
 
@@ -14,6 +23,13 @@ class InvalidPluginManifest(EZPluginInstallerError):
     ...
 
 
+class PluginManifest(BaseModel):
+    name: str
+    package_name: PackageName = Field(alias="package-name")
+    version: Version = Field(alias="version")
+    typing_file: str = Field(alias="typing-file")
+
+
 class EZPluginInstaller(IPluginInstaller):
     info = PluginInstallerInfo.model_construct(
         id="ez.plugins.installer",
@@ -21,7 +37,36 @@ class EZPluginInstaller(IPluginInstaller):
     )
 
     def install(self, path: str) -> PluginInstallationResult:
-        ...
+        path: Path = Path(path)
+
+        if not path.exists():
+            raise FileNotFoundError(path)
+
+        if not path.is_file():
+            raise IsADirectoryError(path)
+        
+        try:
+            zip_file = ZipFile(path)
+        except BadZipFile as e:
+            raise InvalidPluginArchive(path) from e
+        
+        with zip_file:
+            manifest_file = zip_file.open("manifest.yaml")
+            manifest_data = yaml.safe_load(manifest_file)
+            try:
+                manifest = PluginManifest.model_validate(manifest_data)
+            except ValidationError:
+                raise InvalidPluginManifest(path)
+            
+            plugin_dir = self.plugin_dir / manifest.package_name
+            if plugin_dir.exists():
+                raise PluginAlreadyInstalledError(manifest.package_name)
+            plugin_dir.mkdir()
+
+            zip_file.extractall(str(plugin_dir))
+
+            if manifest.typing_file:
+                (plugin_dir / manifest.typing_file).rename()
 
     def uninstall(self, plugin_id: str) -> None:
         ...
