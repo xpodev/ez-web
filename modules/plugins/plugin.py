d4a6767b0db5696e8efbdf3d64f71a512f1ca4e0
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-02-20 18:25:07 +0200

    Remove unused type

--- modules/plugins/plugin.py
+++ modules/plugins/plugin.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from types import ModuleType
 from typing import TypeAlias, TYPE_CHECKING
 
 from .plugin_info import PluginInfo, PluginId
@@ -13,43 +12,6 @@ API_ATTRIBUTE_NAME = "__api__"
 
 
 PluginAPI: TypeAlias = object
-
-
-class EZPlugin:
-    info: PluginInfo
-    module: ModuleType | None
-    enabled: bool
-
-    def __init__(self, info: PluginInfo, module: ModuleType | None):
-        self.info = info
-        self.module = module
-        self.enabled = False
-
-    @property
-    def is_loaded(self) -> bool:
-        return self.module is not None
-
-    @property
-    def name(self) -> str:
-        return self.info.name
-    
-    @property
-    def version(self) -> str:
-        return str(self.info.version)
-    
-    @property
-    def has_api(self) -> bool:
-        return hasattr(self.module, API_ATTRIBUTE_NAME)
-    
-    @property
-    def api(self):
-        return getattr(self.module, API_ATTRIBUTE_NAME, None)
-    
-    def enable(self):
-        self.enabled = True
-
-    def disable(self):
-        self.enabled = False
 
 
 @dataclass
