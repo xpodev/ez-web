287abb5c9d6bb19cf5bb6febd0dec6c339c37586
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-02-22 21:42:59 +0200

    Use jsx.types

--- modules/templates/types.py
+++ modules/templates/types.py
@@ -1,9 +1,1 @@
-from typing import TypeAlias, TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from jsx.html import Element
-    from jsx.components import Component
-
-
-HTML: TypeAlias = str | int | float | bool
-RenderResult: TypeAlias = "Component | Element | HTML"
+from jsx.types import RenderResult
