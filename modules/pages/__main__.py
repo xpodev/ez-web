8eef7057549162a48457a9bcd635c57f60a4a9f0
Neriya Cohen <ee10293847@gmail.com>
2024-02-22 22:07:14 +0200

    Squashed commit of the following:
    commit dfda357d6f81dc886b5220e5c4e716889172b807
    Author: Neriya Cohen <42520786+neriyaco@users.noreply.github.com>
    Date:   Thu Feb 22 22:06:38 2024 +0200
    
        Update __main__.py
    
    commit 95dd815f0b2530d8050e00a573e38b0878712218
    Merge: a18f86e 0f00901
    Author: Binyamin Y Cohen <binyamincohen555@gmail.com>
    Date:   Thu Feb 22 22:06:00 2024 +0200
    
        Merge branch 'dev/add-page-system' of https://github.com/xpodev/ez-web into dev/add-page-system
    
    commit a18f86ebd177ec3186ab328c570c05249e6380f1
    Author: Binyamin Y Cohen <binyamincohen555@gmail.com>
    Date:   Thu Feb 22 22:05:51 2024 +0200
    
        Remove unused import
    
    commit 0f00901acab5835657062382ee7baaf39080cdd1
    Author: Neriya Cohen <42520786+neriyaco@users.noreply.github.com>
    Date:   Thu Feb 22 22:05:38 2024 +0200
    
        Update __main__.py
    
    commit 4f2df9b142091bb188c28013f8b17669d719219f
    Author: Binyamin Y Cohen <binyamincohen555@gmail.com>
    Date:   Thu Feb 22 22:03:22 2024 +0200
    
        Add initial page system
    
    commit 4466878385f4810abad7ef82d097827046ffe775
    Merge: 7f549f2 b97a66b
    Author: Neriya Cohen <42520786+neriyaco@users.noreply.github.com>
    Date:   Thu Feb 22 22:00:47 2024 +0200
    
        Merge pull request #30 from xpodev/dev/small-fixes
    
        Dev/small fixes
    
    commit b97a66b072978af70258e542c676049d8656efb1
    Author: Binyamin Y Cohen <binyamincohen555@gmail.com>
    Date:   Thu Feb 22 21:58:18 2024 +0200
    
        Add template manager priority
    
    commit 1e8904f74f115af08c339d0a52b9870091d215db
    Author: Binyamin Y Cohen <binyamincohen555@gmail.com>
    Date:   Thu Feb 22 21:58:05 2024 +0200
    
        Remove router module (unused)
    
    commit 8a19e376af012b0f02ebe9c760ed34b08de17b1c
    Author: Binyamin Y Cohen <binyamincohen555@gmail.com>
    Date:   Thu Feb 22 21:53:59 2024 +0200
    
        Apply small fixes
    
    commit 4b90c4ef6ccd9e5211c59e182af726fc897f01ac
    Merge: abd75c2 7f549f2
    Author: Binyamin Y Cohen <binyamincohen555@gmail.com>
    Date:   Thu Feb 22 21:52:21 2024 +0200
    
        Merge branch 'main' of https://github.com/xpodev/ez-web into dev/small-fixes
    
    commit abd75c2166aebf64a6b55d7552dcca9675213968
    Author: Binyamin Y Cohen <binyamincohen555@gmail.com>
    Date:   Thu Feb 22 21:49:45 2024 +0200
    
        Merge
    
    commit 7f549f2a5481771aa558acbec319bdfb5bf58b55
    Merge: c2cae44 0047961
    Author: Binyamin Y Cohen <42520501+binyamin555@users.noreply.github.com>
    Date:   Thu Feb 22 21:47:01 2024 +0200
    
        Merge pull request #28 from xpodev/dev/add-templating-system
    
        Add template system
    
    commit 0047961c403a568ade720b99504060ea42659c35
    Author: Binyamin Y Cohen <binyamincohen555@gmail.com>
    Date:   Thu Feb 22 21:43:49 2024 +0200
    
        Change default template root directory
    
    commit 287abb5c9d6bb19cf5bb6febd0dec6c339c37586
    Author: Binyamin Y Cohen <binyamincohen555@gmail.com>
    Date:   Thu Feb 22 21:42:59 2024 +0200
    
        Use jsx.types
    
    commit c2cae4415c7ed1f95aa60b3815b15c9590714857
    Merge: b8de422 b0ecbf4
    Author: Neriya Cohen <42520786+neriyaco@users.noreply.github.com>
    Date:   Thu Feb 22 21:39:39 2024 +0200
    
        Merge pull request #27 from xpodev/dev/add-router-support
    
        Add router support
    
    commit 4bc4ecaad74a71cb1a22d4056ce2f1d396531351
    Author: Binyamin Y Cohen <binyamincohen555@gmail.com>
    Date:   Thu Feb 22 21:22:44 2024 +0200
    
        Add template system

---
+++ modules/pages/__main__.py
@@ -0,0 +1,40 @@
+from functools import wraps
+import ez
+
+from .dbi import PAGE_REPOSITORY, PageInfoModel
+from ez.database import engine
+
+from jsx.html import *
+
+import ez.templates
+
+
+template_manager = ez.templates.get_template_manager()
+
+
+def make_page_route(page: PageInfoModel):
+    if not page.slug.startswith("/"):
+        page.slug = f"/{page.slug}"
+
+    render = template_manager.get(page.template_name).render
+
+    @wraps(render)
+    def page_route(*args, **kwargs):
+        return render(*args, **kwargs)
+    
+    ez.get(page.slug)(page_route)
+
+
+def setup():
+    PAGE_REPOSITORY.connect(engine)
+
+    pages = PAGE_REPOSITORY.all()
+    for page in pages:
+        make_page_route(page)
+
+
+def main():
+    setup()
+
+
+main()
