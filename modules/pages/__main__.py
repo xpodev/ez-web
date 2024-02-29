77a850f2ff286bca560c30d5246b79e93e96f114
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-02-29 16:44:54 +0200

    Fix page data injection code

--- modules/pages/__main__.py
+++ modules/pages/__main__.py
@@ -1,4 +1,5 @@
-from functools import wraps
+import inspect
+
 import ez
 
 from .dbi import PAGE_REPOSITORY, PageInfoModel
@@ -13,12 +14,37 @@ def make_page_route(page: PageInfoModel):
     if not page.slug.startswith("/"):
         page.slug = f"/{page.slug}"
 
-    render = ez.templates.get(page.template_name).render
+    template = ez.templates.get(page.template_name)
+    signature = inspect.signature(template.render)
+    inject_kwargs = {}
+    parameters = []
+    for param in signature.parameters.values():
+        annotation = param.annotation
+        if annotation is inspect.Parameter.empty:
+            continue
+        if isinstance(annotation, type) and issubclass(annotation, ez.templates.PageData):
+            if param.kind in { param.POSITIONAL_OR_KEYWORD, param.KEYWORD_ONLY }:
+                inject_kwargs[param.name] = ez.templates.PageData(
+                    id=page.id,
+                    title=page.title,
+                    content=page.content,
+                    slug=page.slug,
+                    template_name=page.template_name
+                )
+            elif param.kind == param.POSITIONAL_ONLY:
+                raise ValueError("Page parameter may not be positional-only")
+            else:
+                raise ValueError(f"Invalid parameter kind for page: {param.kind}")
+        else:
+            parameters.append(param)
 
-    @wraps(render)
+    new_signature = signature.replace(parameters=parameters)
+
     def page_route(*args, **kwargs):
-        return render(*args, **kwargs)
+        return template.render(*args, **inject_kwargs, **kwargs)
     
+    page_route.__signature__ = new_signature
+
     ez.get(page.slug)(page_route)
 
 
