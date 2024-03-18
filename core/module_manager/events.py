e461ea7e78116cb47605b2982c20172824694187
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-03-18 22:47:33 +0200

    Add core module manager

---
+++ core/module_manager/events.py
@@ -0,0 +1,13 @@
+from utilities.event import Event
+
+
+class Modules(Event):
+    WillLoad = "Modules.WillLoad"
+    """
+    This event is emitted before loading the modules.
+    """
+
+    DidLoad = "Modules.DidLoad"
+    """
+    This event is emitted after loading the modules.
+    """
