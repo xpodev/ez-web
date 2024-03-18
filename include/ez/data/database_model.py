5818b0c916e2fc40eef2a7fb4a44e659231d5213
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-03-18 22:52:38 +0200

    Move ez.data to include

--- include/ez/data/database_model.py
+++ include/ez/data/database_model.py
@@ -5,7 +5,7 @@ from typing import Any, ClassVar
 from sqlalchemy import Column
 from sqlalchemy.orm import DeclarativeBase
 
-from ez.config import config
+from ez.site import CONFIG
 
 
 class DatabaseModel(DeclarativeBase):
@@ -20,5 +20,5 @@ class DatabaseModel(DeclarativeBase):
             table_name = cls.__name__.rstrip("Model").lower() + "s"
             warnings.warn(f"__tablename__ not set for {cls.__name__}, defaulting to {table_name}")
             cls.__tablename__ = table_name
-        cls.__tablename__ = config.database.prefix + cls.__tablename__
+        cls.__tablename__ = CONFIG.database.prefix + cls.__tablename__
         super().__init_subclass__(**kw)
