054e3d0d3ffc694539b18a2d352500bf25e5435e
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-04-08 23:15:50 +0300

    Fix ez pages module

--- modules/pages/__main__.py
+++ modules/pages/__main__.py
@@ -3,14 +3,14 @@ import inspect
 import ez
 
 from ez.database import engine
+import ez.web
 
 from .dbi import PAGE_REPOSITORY, PageInfoModel, PAGES_HISTORY_REPOSITORY
 from .router import pages_api_router
 
-from fastapi.responses import HTMLResponse
+from starlette.responses import HTMLResponse
 
 
-from jsx.html import *
 from jsx.renderer import render
 
 import ez.templates
@@ -21,35 +21,36 @@ def make_page_route(page: PageInfoModel):
 
 
     template = ez.templates.get(page.template_name)
-    signature = inspect.signature(template.render)
-    inject_kwargs = {}
-    parameters = []
-    for param in signature.parameters.values():
-        annotation = param.annotation
-        if annotation is inspect.Parameter.empty:
-            parameters.append(param)
-        elif isinstance(annotation, type) and issubclass(annotation, ez.templates.PageData):
-            if param.kind in { param.POSITIONAL_OR_KEYWORD, param.KEYWORD_ONLY }:
-                inject_kwargs[param.name] = ez.templates.PageData(
-                    id=page.id,
-                    title=page.title,
-                    content=page.content,
-                    slug=page.slug,
-                    template_name=page.template_name
-                )
-            elif param.kind == param.POSITIONAL_ONLY:
-                raise ValueError("Page parameter may not be positional-only")
-            else:
-                raise ValueError(f"Invalid parameter kind for page: {param.kind}")
-        else:
-            parameters.append(param)
 
-    new_signature = signature.replace(parameters=parameters)
+    # signature = inspect.signature(template.render)
+    # inject_kwargs = {}
+    # parameters = []
+    # for param in signature.parameters.values():
+    #     annotation = param.annotation
+    #     if annotation is inspect.Parameter.empty:
+    #         parameters.append(param)
+    #     elif isinstance(annotation, type) and issubclass(annotation, ez.templates.PageData):
+    #         if param.kind in { param.POSITIONAL_OR_KEYWORD, param.KEYWORD_ONLY }:
+    #             inject_kwargs[param.name] = ez.templates.PageData(
+    #                 id=page.id,
+    #                 title=page.title,
+    #                 content=page.content,
+    #                 slug=page.slug,
+    #                 template_name=page.template_name
+    #             )
+    #         elif param.kind == param.POSITIONAL_ONLY:
+    #             raise ValueError("Page parameter may not be positional-only")
+    #         else:
+    #             raise ValueError(f"Invalid parameter kind for page: {param.kind}")
+    #     else:
+    #         parameters.append(param)
 
-    def page_route(*args, **kwargs):
-        return HTMLResponse(render(template.render(*args, **inject_kwargs, **kwargs)))
+    # new_signature = signature.replace(parameters=parameters)
+
+    def page_route(request):
+        return HTMLResponse(render(template.render(page=page)))
     
-    page_route.__signature__ = new_signature
+    # page_route.__signature__ = new_signature
 
     ez.web.get(page.slug)(page_route)
 
@@ -63,11 +64,8 @@ def setup():
         make_page_route(page)
 
 
-def main():
-    setup()
+setup()
 
-
-main()
 
 ez.web.add_router("/api/pages", pages_api_router)
 
