b15068353911b6030c1192de9657b51a46c8f0e4
Neriya Cohen <ee10293847@gmail.com>
2024-04-20 23:21:16 +0300

    Fix seamless integration

--- modules/ezjsx/__main__.py
+++ modules/ezjsx/__main__.py
@@ -1,16 +1,12 @@
 import ez
 import ez.lowlevel
 
-from jsx.server import JSXServer
+from jsx.middlewares import ASGIMiddleware
 from jsx.renderer import render
 from jsx.components import Component
 from jsx.html import Element
 from ez.events import HTTP
 from .events import TreeRenderer
-
-
-jsx_server = JSXServer()
-
 
 @ez.events.on(HTTP.Out)
 def render_tree(_):
@@ -28,6 +24,6 @@ def render_tree(_):
         ez.response.html(result)
 
 
-jsx_server.mount(ez.lowlevel.WEB_APP)
+ez.lowlevel.WEB_APP.add_middleware(ASGIMiddleware)
 
 __module_name__ = "EZ JSX Integration"
