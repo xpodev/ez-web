63b1e0b5fe470fee41f9d2fd79169156c9e5e145
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-04-21 21:02:39 +0300

    Add calling application capability to syscall

--- include/sandbox/host/utils.py
+++ include/sandbox/host/utils.py
@@ -8,17 +8,21 @@ P = ParamSpec("P")
 T = TypeVar("T")
 
 @overload
-def syscall(fn: Callable[P, T]) -> Callable[P, T]: ...
+def syscall(fn: Callable[P, T], pass_calling_application: bool = False) -> Callable[P, T]: ...
 @overload
-def syscall(fn: T) -> T: ...
+def syscall(fn: T, pass_calling_application: bool = False) -> T: ...
 
-def syscall(fn: Any) -> Any:
+def syscall(fn: Any, pass_calling_application: bool = False) -> Any:
     current_host = AppHost.current_host
     current_application = current_host.current_application
 
     @wraps(fn)
     def wrapper(*args: P.args, **kwargs: P.kwargs):
+        if pass_calling_application:
+            injected_args = (current_host.current_application,)
+        else:
+            injected_args = ()
         with current_host._context.application(current_application):
-            return fn(*args, **kwargs)
+            return fn(*injected_args, *args, **kwargs)
 
     return wrapper
