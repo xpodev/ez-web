94c4d8aed56ca992cb90aab15fb2c52be7b29e6f
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-04-21 21:07:32 +0300

    Move plugin manager init code to main

--- modules/plugins/__init__.py
+++ modules/plugins/__init__.py
@@ -1,27 +1,4 @@
-import ez.lowlevel
-
-from .machinery.manager import PluginManager
-from .builtins.installer import EZPluginInstaller
-from .builtins.loader import EZPluginLoader
-
-from .config import PLUGINS_DIRECTORY
-
-from . import manager
-
-
-def init_manager(host):
-    manager.PLUGIN_MANAGER = PluginManager(
-        host, 
-        str(PLUGINS_DIRECTORY), 
-        EZPluginInstaller, 
-        EZPluginLoader
-    )
-    manager.PLUGIN_MANAGER.enable_public_api()
-    return manager.PLUGIN_MANAGER
-
-
-init_manager(ez.lowlevel.APP_HOST)
-
 __deps__ = [
-    "events"
+    "events",
+    "site",
 ]
