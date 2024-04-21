941ccb0aa22c20da923667dffb07eec52e861a9f
Neriya Cohen <ee10293847@gmail.com>
2024-04-21 20:36:44 +0300

    Change log to be include only

--- modules/log/__api__/__init__.py
+++ modules/log/__api__/__init__.py
@@ -1,27 +1,3 @@
-from .. import logger, _style, colors
+from ez.log import info, debug, warning, error, critical
 
-
-def debug(*args, sep=' '):
-    msg = sep.join(map(str, args))
-    msg = f"{_style('DEBUG', colors['blue'])}:".ljust(19) + msg
-    logger.debug(msg)
-
-def info(*args, sep=' '):
-    msg = sep.join(map(str, args))
-    msg = f"{_style('INFO', colors['green'])}:".ljust(19) + msg
-    logger.info(msg)
-
-def warning(*args, sep=' '):
-    msg = sep.join(map(str, args))
-    msg = f"{_style('WARNING', colors['yellow'])}:".ljust(19) + msg
-    logger.warning(msg)
-
-def error(*args, sep=' '):
-    msg = sep.join(map(str, args))
-    msg = f"{_style('ERROR', colors['red'])}:".ljust(19) + msg
-    logger.error(msg)
-
-def critical(*args, sep=' '):
-    msg = sep.join(map(str, args))
-    msg = f"{_style('CRITICAL', colors['red'])}:".ljust(19) + msg
-    logger.critical(msg)
+__all__ = ["info", "debug", "warning", "error", "critical"]
