aa8cdf034cff92ccb7d2d91d32b23ca4425e1f9e
Neriya Cohen <ee10293847@gmail.com>
2024-04-23 23:37:02 +0300

    Fix database app started event

--- include/ez/database/__init__.py
+++ include/ez/database/__init__.py
@@ -1,13 +1,12 @@
 import ez
 from ez.site import CONFIG
-from web.app.events import App
 from sqlalchemy import *
 from sqlalchemy.orm import Session
 
 engine = create_engine(CONFIG.database.uri, pool_recycle=3600)
 session = Session(engine)
 
-@ez.events.on(App.DidStart)
+@ez.events.on("App.Started")
 def close_session():
     global session, engine
 
