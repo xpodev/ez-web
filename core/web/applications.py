964e2370e1cb45dec0b0b098988ac7aef8188b49
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-05-22 15:31:00 +0300

    Remove EZ middleware

--- core/web/applications.py
+++ core/web/applications.py
@@ -1,21 +1,9 @@
 from typing import TYPE_CHECKING
 
 from starlette.applications import Starlette
-from starlette.middleware.base import BaseHTTPMiddleware
-
 
 if TYPE_CHECKING:
     from sandbox.host import AppHost
-
-
-class EZMiddleware(BaseHTTPMiddleware):
-    def __init__(self, app, host: "AppHost"):
-        super().__init__(app)
-        self.host = host
-
-    async def dispatch(self, request, call_next):
-        with self.host.application(self.app):
-            return await call_next(request)
 
 
 class EZWebApplication(Starlette):
