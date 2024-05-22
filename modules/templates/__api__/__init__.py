2089dc04583a7f2d741d44006e716991dcb07288
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-05-22 14:36:06 +0300

    Fix API typing

--- modules/templates/__api__/__init__.py
+++ modules/templates/__api__/__init__.py
@@ -1,9 +1,23 @@
+from typing import TYPE_CHECKING
 
-from ..errors import TemplateNotFoundError
-from ..manager import TEMPLATE_MANAGER as __tm
-from ..template_package import TemplatePackage
-from ..template_pack import TemplatePack
-from ..template import Template, template
+from pydantic import BaseModel
+
+
+if TYPE_CHECKING:
+    from modules.templates.errors import TemplateNotFoundError
+    from modules.templates.template_package import TemplatePackage
+    from modules.templates.template_pack import TemplatePack
+    from modules.templates.template import Template, template
+else:
+    from ..errors import TemplateNotFoundError
+    from ..manager import TEMPLATE_MANAGER as __tm
+    from ..template_package import TemplatePackage
+    from ..template_pack import TemplatePack
+    from ..template import Template, template
+
+
+class Empty(BaseModel):
+    pass
 
 
 def get(name: str) -> "TemplatePack | Template":
