711a179afee83171207b671990ba52db747457ac
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-05-22 14:36:59 +0300

    Fix ez_template to use functional template

--- modules/templates/builtins/ez_template.py
+++ modules/templates/builtins/ez_template.py
@@ -1,16 +1,14 @@
-from ..template import Template
+from ..template import FunctionalTemplate
 from ..template_pack import TemplatePack
 
 from .template_module import TemplateModule
 
 
-class EZTemplate(Template):
+class EZTemplate(FunctionalTemplate):
     _module: TemplateModule
 
     def __init__(
         self, name: str, module: TemplateModule, parent: TemplatePack | None = None
     ) -> None:
-        super().__init__(name, parent)
+        super().__init__(name, module.render, parent)
         self._module = module
-
-        self.render = self._module.render
