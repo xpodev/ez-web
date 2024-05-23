d0c7076c216af4ab6e921b04bee35570e85f1592
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-05-23 17:23:30 +0300

    Remove session middleware

--- modules/admin/router.py
+++ modules/admin/router.py
@@ -3,16 +3,13 @@ from starlette.responses import JSONResponse, PlainTextResponse
 
 import ez
 
-from starlette.middleware.sessions import SessionMiddleware
-from starlette.middleware import Middleware
-
 from .context import get_current_session, close_session
 from .requests import LoginRequest
 
 from .authentication import authenticate
 
 
-admin = ez.web.http.router("/admin", middleware=[Middleware(SessionMiddleware, secret_key="ez-admin")])
+admin = ez.web.http.router("/admin")
 
 
 @admin.post("/login")
@@ -20,26 +17,12 @@ async def login(request: Request):
     async with request.form() as form:
         login = LoginRequest.model_validate(form)
 
-        session = authenticate(login.username, login.password)
-        if session is None:
-            raise ez.web.http.HTTPException(ez.web.http.HTTPStatus.UNAUTHORIZED, "Invalid credentials")
-
-        request.session["ez-admin:sid"] = session.id
-
         return JSONResponse({"message": "Logged in"})
     
 
 @admin.post("/logout")
 def logout(request: Request):
     session = get_current_session()
-    if close_session(session):
-        del request.session["ez-admin:sid"]
+    close_session(session)
     return JSONResponse({"message": "Logged out"})
     
-
-@admin.get("/test")
-def test(request: Request):
-    session = get_current_session()
-    return PlainTextResponse(
-        session.id + " :: " + str(session.user)
-    )
