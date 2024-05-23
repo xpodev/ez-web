8a47e5e6b40ca0eb875fa2b0484579a83b6ff19e
Binyamin Y Cohen <binyamincohen555@gmail.com>
2024-05-23 20:30:44 +0300

    Fix not implemented routes to raise an error

--- modules/plugins/router.py
+++ modules/plugins/router.py
@@ -16,15 +16,17 @@ def get_plugins(_):
 
 @router.post("/{plugin_id}/enable")
 def enable_plugin(request):
-    plugin_id = request.path_params["plugin_id"]
-    print(f"Enabling plugin {plugin_id}...")
-    ez.plugins.enable_plugin(plugin_id)
-    return "Plugin enabled!"
+    # TODO: implement correctly
+    # plugin_id = request.path_params["plugin_id"]
+    # ez.plugins.enable_plugin(plugin_id)
+    # return "Plugin enabled!"
+    raise NotImplementedError
 
 
 @router.post("/{plugin_id}/disable")
 def disable_plugin(request):
-    plugin_id = request.path_params["plugin_id"]
-    print(f"Disabling plugin {plugin_id}...")
-    ez.plugins.disable_plugin(plugin_id)
-    return "Plugin disabled!"
+    # TODO: implement correctly
+    # plugin_id = request.path_params["plugin_id"]
+    # ez.plugins.disable_plugin(plugin_id)
+    # return "Plugin disabled!"
+    raise NotImplementedError
